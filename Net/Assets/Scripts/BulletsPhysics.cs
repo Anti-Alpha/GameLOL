f270ef096e80e860d3f83086c2447c0a6d675962
Anti-Alpha <nikita.lebedintsev@yandex.ru>
2019-05-18 13:35:15 +0300

    Update bullet physics

---
+++ Net/Assets/Scripts/BulletsPhysics.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletsPhysics : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    float f = 0;
+    void Update()
+    {
+        gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, f));
+        f += 4;
+    }
+}
